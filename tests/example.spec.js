80d36e6dbe10017218e4a4a17c6aacbc4f097aaa
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-05-09 14:53:26 +0600

    Tag Test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,13 +1,13 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-test('First Test', async ({ page }) => {
+test("First Test @regression", async ({ page }) => {
   //Writing testing code here
   await page.goto('https://playwright.dev/');
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
 });
 
-test("Simple click test", async ({ page, browserName }) => {
+test("Simple click test @regression", async ({ page, browserName }) => {
   test.skip(browserName === 'firefox', 'Working on the firefox fix');
   await page.goto("https://the-internet.herokuapp.com/");
   const element = page.locator("text=Add/Remove Elements");
@@ -16,7 +16,15 @@ test("Simple click test", async ({ page, browserName }) => {
   await addElement.click();
 })
 
-test("Duplicate Test", async ({ page }) => {
+test("Duplicate Test 1 @smoke", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  const element = page.locator("text=Add/Remove Elements");
+  const addElement = page.locator("text=Add Element");
+  await element.click();
+  await addElement.click();
+})
+
+test("Duplicate Test 2 @regression", async ({ page }) => {
   await page.goto("https://the-internet.herokuapp.com/");
   const element = page.locator("text=Add/Remove Elements");
   const addElement = page.locator("text=Add Element");

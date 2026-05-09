767b8df6c8ac4c9fd41fb32aa064bd944009b21c
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-05-09 20:45:05 +0600

    Parallel Mode

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,14 +1,11 @@
 const { test, expect } = require('@playwright/test');
 
-test.describe("Smoke Tests", () => {
+test.describe.parallel("Smoke Tests", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
   })
-  test.afterEach(async ({ page }) => {
-    console.log("This test is finished");
-  })
 
-  test("Simple click test @regression", async ({ page, browserName }) => {
+  test("Simple click test", async ({ page, browserName }) => {
     await page.pause();
     const element = page.locator("text=Add/Remove Elements");
     const addElement = page.locator("text=Add Element");
@@ -16,14 +13,14 @@ test.describe("Smoke Tests", () => {
     await addElement.click();
   })
 
-  test("Duplicate Test 1 @smoke", async ({ page }) => {
+  test("Duplicate Test 1", async ({ page }) => {
     const element = page.locator("text=Add/Remove Elements");
     const addElement = page.locator("text=Add Element");
     await element.click();
     await addElement.click();
   })
 
-  test("Duplicate Test 2 @regression", async ({ page }) => {
+  test("Duplicate Test 2", async ({ page }) => {
     const element = page.locator("text=Add/Remove Elements");
     const addElement = page.locator("text=Add Element");
     await element.click();

0d6fd33d3b5c756eb6a750ac498dcc236cce360e
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-05-09 13:41:46 +0600

    Playwright Locator Example

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,15 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-test('First Test', async ({ page }) => {
-  //Writing testing code here
-  await page.goto('https://playwright.dev/');
+// test('First Test', async ({ page }) => {
+//   //Writing testing code here
+//   await page.goto('https://playwright.dev/');
+//   const title = page.locator('.navbar__inner .navbar__title');
+//   await expect(title).toHaveText('Playwright');
+// });
+
+test("Simple click test", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.locator("text=Add/Remove Elements").click();
   await page.pause();
-});
+  await page.locator("text=Add Element").click();
+})
